a70cbb1f8f60fadc2e35f7c1348f2d947f82f6d6
svpkhsriramvarma <142596357+svpkhsriramvarma@users.noreply.github.com>
2024-01-06 18:17:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -81,6 +81,7 @@ const checkWinner = () => {
         if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
             if(pos1Val === pos2Val && pos2Val === pos3Val) {
                 showWinner(pos1Val);
+                return true;
             }
         }
     }
